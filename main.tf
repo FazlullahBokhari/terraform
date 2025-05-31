18c277d2c191c5a93145df018b9abb9f47bc33b0
FazlullahBokhari <fazlullahb@gmail.com>
2025-06-01 00:30:07 +0530

    added  test.tfvars from Git tracking

--- main.tf
+++ main.tf
@@ -2,9 +2,10 @@ provider "aws" {
   region = var.aws_region
 }
 
-
 resource "aws_instance" "name" {
   ami           = var.ami_id
   instance_type = "t2.micro"
+
+  # Optional: Uncomment only if key exists
   key_name      = "terraform"
 }
